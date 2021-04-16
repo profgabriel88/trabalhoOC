371f21b98855ae3feedb031653217c62968f39ae
profgabriel88 <59454245+profgabriel88@users.noreply.github.com>
2021-04-16 10:59:10 -0400

    Update grayCode.cpp

--- grayCode.cpp
+++ grayCode.cpp
@@ -5,50 +5,58 @@
 using namespace std;
 
 int main() {
-
+    // cria um objeto da classe Input File Stream - Arquivo de Entrada
     ifstream valoresDeEntrada("lista.txt");
+    // variável string para armazenar cada linha do arquivo
     string linha;
 
+    // cria um objeto da classe Output File Stream - Arquivo de Saída
     ofstream valoresDeSaida("listaConvertida.txt");
 
+    // itera todas as linhas do arquivo de entrada
     while (getline(valoresDeEntrada, linha))
     {
-        int abacaxi = stoi(linha), i = 0;
+        // recebe o valor da string e converte para inteiro
+        int valorDecimal = stoi(linha);
+        int i = 0; // variável para iterar a conversão decimal -> binário
+        
+        // vetores para armazenar os bits binários e gray
         unsigned int n = 6;
-        unsigned int abacaBin[n] = {0};
-        unsigned int abacaGray[n] = {0};
+        unsigned int valorBinario[n] = {0};
+        unsigned int valorGray[n] = {0};
 
-        valoresDeSaida << "Valor de entrada: " << abacaxi << ", ";
-        //cout << "\n";
+        // escreve no arquivo de saída o valor de entrada
+        valoresDeSaida << "Valor de entrada: " << valorDecimal << ", ";
 
-        while (abacaxi > 0)
+        // conversão de decimal para binário por sucessivas divisões por 2
+        while (valorDecimal > 0)
         {
-            abacaBin[i] = abacaxi%2;
-            abacaxi = abacaxi/2;
+            valorBinario[i] = valorDecimal%2;
+            valorDecimal = valorDecimal/2;
             i++;    
         }
         
-        
+        // converte os binários para código gray
         for (int j = n-1; j >= 0; j--)
         {
-            if (j == n-1) abacaGray[j] == abacaBin[j];
-
-            else
-            {
-                abacaGray[j] = abacaBin[j+1]^abacaBin[j];
-            }
-            //cout << abacaGray[j];
+            // o MSB é igual para os dois
+            if (j == n-1) valorGray[j] = valorBinario[j];
             
+            // os outros bits são obtidos pela operação XOR (^) da seguinte forma
+            // 1º XOR 2º, 2º XOR 3º, nbit XOR n+1bit
+            else valorGray[j] = valorBinario[j+1]^valorBinario[j];
         }
 
+        // escreve no arquivo de saída os valores binários
         valoresDeSaida << "código binário: ";
         for (int i = n-1; i >= 0; i--) 
-            valoresDeSaida << abacaBin[i];
+            valoresDeSaida << valorBinario[i];
         valoresDeSaida << ", ";
 
+        // escreve no arquivo de saída os valores gray
         valoresDeSaida << "código gray: ";
         for (int i = n-1; i >= 0; i--) 
-            valoresDeSaida << abacaGray[i];
+            valoresDeSaida << valorGray[i];
         valoresDeSaida << "\n";
     }
     return 0;
